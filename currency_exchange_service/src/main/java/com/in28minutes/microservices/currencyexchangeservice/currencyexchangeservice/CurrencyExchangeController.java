54f177acde1c3549d620415c4c0d3b5551c69bbc
Yiwei Wu <yiweiwu0128@gmail.com>
2024-02-04 20:07:17 +1100

    create a JPA repository

--- currency_exchange_service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/currencyexchangeservice/CurrencyExchangeController.java
+++ currency_exchange_service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/currencyexchangeservice/CurrencyExchangeController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
+    @Autowired
+    private CurrencyExchangeRepository repository;
     @Autowired
     private Environment environment;
 
     // we create the from and to as path variables, so others can path into variables they want
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null)
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
+
         return currencyExchange;
     }
 }
---
+++ currency_exchange_service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.microservices.currencyexchangeservice.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    // can do a findBy + the column name that you want to find by:
+    // so spring data jpa will process will convert this to a query
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
