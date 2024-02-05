1db22c7aca53349612f59acd53e2882b009d695a
Yiwei Wu <yiweiwu0128@gmail.com>
2024-02-05 14:53:41 +1100

    setting up Eureka Naming Server

---
+++ naming-server/src/main/java/com/in28minutes/microservice/namingserver/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservice.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NamingServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServerApplication.class, args);
+    }
+}
