a308657f6181488e89515b6c0b37e56c81b7c674
Yiwei Wu <yiweiwu0128@gmail.com>
2024-02-07 17:30:16 +1100

    exploring rate liting and bulkhead features of resilience4j

--- currency_exchange_service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency_exchange_service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,27 +1,27 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
-    private Logger logger =
-            LoggerFactory.getLogger(CircuitBreakerController.class);
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
     // what it does is when a request fails, it will retry again, and if it fails three times, it will return error back
     // @Retry(name = "default")
     // @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // @RateLimiter(name = "default") // in the given 10s => we want to only allow 10000 calls to the sample api
+    @Bulkhead(name = "sample-api")
     public String sampleApi() {
-        logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();
+//        logger.info("Sample Api call received");
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex) {
